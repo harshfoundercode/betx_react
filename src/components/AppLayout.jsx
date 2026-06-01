01c9fac0243512969eb6fb53d5da2b743ad7f516
harshfoundercode <Harsh@foundercodes.com>
2026-06-01 12:02:39 +0530

    all

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,37 +1,12 @@
-// import { Outlet } from "react-router-dom";
-// import BottomNav from "./BottomNav";
-
-// export default function AppLayout() {
-//   return (
-//     <div className="min-h-screen bg-black">
-//       <div className="pb-24">
-//         <Outlet />
-//       </div>
-
-//       <BottomNav />
-//     </div>
-//   );
-// }
-
 import { Outlet } from "react-router-dom";
 import BottomNav from "./BottomNav";
 
 export default function AppLayout() {
   return (
-    <div
-      className="
-        min-h-screen
-        bg-black
-        text-white
-        overflow-x-hidden
-      "
-      style={{
-        backgroundColor: "#000",
-      }}
-    >
-      <main className="pb-24">
+    <div className="min-h-screen bg-black">
+      <div className="pb-24">
         <Outlet />
-      </main>
+      </div>
 
       <BottomNav />
     </div>
