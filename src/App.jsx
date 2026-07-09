ce05444aec0943af42abcd683464883bfc4e9221
harshfoundercode <Harsh@foundercodes.com>
2026-07-09 18:08:30 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-
 import { AuthProvider } from "./pages/services/AuthContext";
 import PublicRoute from "./components/PublicRoutes";
-
+import { SocketProvider } from "./shared/socket/SocketContext"; // Import SocketProvider
 import AuthPage from "./pages/authscreens/AuthPage";
 import RegisterPage from "./pages/RegisterScreen";
-
 import AppLayout from "./components/AppLayout";
-
 import HomePage from "./pages/HomePage";
 import DepositScreen from "./pages/DepositScreen";
 import WithdrawScreen from "./pages/WithdrawScreen";
@@ -20,62 +17,90 @@ import ProfileScreen from "./pages/ProfileScreen";
 import VipPage from "./pages/VipPage";
 import ViewAllGamesPage from "./pages/GameViewAll";
 import AccountStatementScreen from "./pages/TransactionHistry";
+import WinGo from "./Lottery/WinGo";
+import AvitatorLayout from "./AviatorGame/AvitatorLayout";
+import AviatorHome from "./AviatorGame/AviatorHome";
+// import Game from "./ChickenRoadGame/GameComponent/Game";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-
-          {/* Auth Pages */}
-          <Route
-            path="/login"
-            element={
-              <PublicRoute>
-                <AuthPage />
-              </PublicRoute>
-            }
-          />
-
-          <Route
-            path="/register"
-            element={
-              <PublicRoute>
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
-
-          {/* Pages with BottomNav */}
-          <Route
-            element={
-              <PublicRoute>
-                <AppLayout />
-              </PublicRoute>
-            }
-          >
-            <Route path="/" element={<HomePage />} />
-            <Route path="/deposit" element={<DepositScreen />} />
-            <Route path="/withdraw" element={<WithdrawScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-            <Route path="/vip" element={<VipPage />} />
+        <SocketProvider> {/* Wrap everything with SocketProvider */}
+          <Routes>
+            {/* Auth Pages */}
             <Route
-              path="/view-all-games"
-              element={<ViewAllGamesPage />}
+              path="/login"
+              element={
+                <PublicRoute>
+                  <AuthPage />
+                </PublicRoute>
+              }
             />
-          </Route>
 
-          <Route
-            path="/account-statement"
-            element={
-              <PublicRoute>
-                <AccountStatementScreen />
-              </PublicRoute>
-            }
-          />
-        
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
 
-        </Routes>
+            {/* Pages with BottomNav */}
+            <Route
+              element={
+                <PublicRoute>
+                  <AppLayout />
+                </PublicRoute>
+              }
+            >
+              <Route path="/" element={<HomePage />} />
+              <Route path="/deposit" element={<DepositScreen />} />
+              <Route path="/withdraw" element={<WithdrawScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/vip" element={<VipPage />} />
+              <Route
+                path="/view-all-games"
+                element={<ViewAllGamesPage />}
+              />
+            </Route>
+
+            <Route
+              path="/account-statement"
+              element={
+                <PublicRoute>
+                  <AccountStatementScreen />
+                </PublicRoute>
+              }
+            />
+            
+            <Route
+              path="/lottery/wingo"
+              element={
+                <PublicRoute>
+                  <WinGo />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/aviator"
+              element={
+                <PublicRoute>
+                  <AviatorHome />
+                </PublicRoute>
+              }
+            />
+            {/* <Route
+              path="/chickenRoadGame"
+              element={
+                <PublicRoute>
+                  <Game />
+                </PublicRoute>
+              }
+            /> */}
+          </Routes>
+        </SocketProvider>
       </AuthProvider>
     </Router>
   );
